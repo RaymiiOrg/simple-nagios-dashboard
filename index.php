9b545eb29415eb0e8f62a3da1393031a558db160
Garet McKinley <garetmckinley@me.com>
2015-08-26 23:27:15 -0400

    Wrapped the dash in a container element
    I also fixed indentation issues.

--- index.php
+++ index.php
@@ -34,67 +34,67 @@ require_once("./functions.php");
     </style>
 </head>
 <body>
-
-    <div class="row">
-        <div class="col-md-12">
-            <h1><?php print($title . " - " . $organization); ?></h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1><?php print($title . " - " . $organization); ?></h1>
+            </div>
         </div>
-    </div>
 
-    <div class="row">
-        <div class="col-md-12">
-            <?php
-            print($hosts_total . " Hosts");
-            print("<br />" . $service_total . " Services<br />");
-            if($criticals_count > 0 || $host_issue_count > 0) {
-                print('<div class="alert alert-danger"><h2>');
-                    #print('<script type="text/javascript">document.body.style.backgroundColor = "#ff0039";</script>');
-                if($criticals_count > 0) {
-                    print(($criticals_count));
-                    print(" Critical Issues. ");
+        <div class="row">
+            <div class="col-md-12">
+                <?php
+                print($hosts_total . " Hosts");
+                print("<br />" . $service_total . " Services<br />");
+                if($criticals_count > 0 || $host_issue_count > 0) {
+                    print('<div class="alert alert-danger"><h2>');
+#print('<script type="text/javascript">document.body.style.backgroundColor = "#ff0039";</script>');
+                    if($criticals_count > 0) {
+                        print(($criticals_count));
+                        print(" Critical Issues. ");
+                    }
+                    if ($host_issue_count > 0) {
+                        print($host_issue_count . " Hosts Down!");
+                    } 
+                    print("<h2></div>");
+                } elseif(($warnings_count) > 0) {
+                    print(($warnings_count));
+                    print(" Non Critical Issues</h2></div>");
+                } else {
+                    print('<div class="alert alert-success"><h2>');
+                    print(" Everything Running Fine!</h2></div>");
                 }
-                if ($host_issue_count > 0) {
-                 print($host_issue_count . " Hosts Down!");
-             } 
-             print("<h2></div>");
-         } elseif(($warnings_count) > 0) {
-            print(($warnings_count));
-            print(" Non Critical Issues</h2></div>");
-        } else {
-            print('<div class="alert alert-success"><h2>');
-            print(" Everything Running Fine!</h2></div>");
-        }
-        ?>
-    </div>
-</div>
+                ?>
+            </div>
+        </div>
 
-<div class="row">
-    <div class="col-md-6">
-        <?php
-        host_alert_cards("Hosts Down", "danger", $host_issue_count, $host_issues);
-        service_alert_cards("Critical Issues", "danger", $criticals_count, $criticals);
-        host_alert_cards("Hosts Acknowledged Down", "info", $host_ack_issues_count, $host_ack_issues);
-        ?>
-    </div>
-    <div class="col-md-6">
-        <?php
-        service_alert_cards("Minor Issues ", "warning", $warnings_count, $warnings);
-        service_alert_cards("Acknowledged Minor Issues", "info", $warnings_ack_count, $warnings_ack_issues);
-        service_alert_cards("Acknowledged Criticals", "info", $criticals_ack_count, $criticals_ack);
-        ?>
-    </div>
-    <div class="col-md-6">
-        <?php
-        ?>
-    </div>
-</div>
-<br />
-<div class="row">
-    <div class="container text-center">
-    <span>Simple Nagios Dashboard by <a href="https://raymii.org">Remy van Elst (code)</a> and <a href="https://github.com/JobV">Job van der Voort (design)</a>.</span>
-    </div>
-</div>
+        <div class="row">
+            <div class="col-md-6">
+                <?php
+                host_alert_cards("Hosts Down", "danger", $host_issue_count, $host_issues);
+                service_alert_cards("Critical Issues", "danger", $criticals_count, $criticals);
+                host_alert_cards("Hosts Acknowledged Down", "info", $host_ack_issues_count, $host_ack_issues);
+                ?>
+            </div>
+            <div class="col-md-6">
+                <?php
+                service_alert_cards("Minor Issues ", "warning", $warnings_count, $warnings);
+                service_alert_cards("Acknowledged Minor Issues", "info", $warnings_ack_count, $warnings_ack_issues);
+                service_alert_cards("Acknowledged Criticals", "info", $criticals_ack_count, $criticals_ack);
+                ?>
+            </div>
+            <div class="col-md-6">
+                <?php
+                ?>
+            </div>
+        </div>
+        <br />
+        <div class="row">
+            <div class="container text-center">
+                <span>Simple Nagios Dashboard by <a href="https://raymii.org">Remy van Elst (code)</a> and <a href="https://github.com/JobV">Job van der Voort (design)</a>.</span>
+            </div>
+        </div>
 
-
+    </div>
 </body>
 </html>
